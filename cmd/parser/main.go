5d0f9a74ec568fe20f5246be14b6cd55ba951d92
nori266 <noriel266@gmail.com>
2020-02-28 16:52:30 +0200

    added json output

--- cmd/parser/main.go
+++ cmd/parser/main.go
@@ -4,12 +4,18 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"flag"
+	"encoding/json"
 
 	"speechly/nlu-rules-parser/pkg/nlurules"
 )
 
 func main() {
+
 	fname := os.Args[1]
+
+	jsonFlag := flag.Bool("-json", true, "return results as json")
+	flag.Parse()
 
 	f, err := os.Open(fname)
 	if err != nil {
@@ -24,12 +30,17 @@ func main() {
 
 		for scan.Scan() {
 			line := scan.Text()
-			fmt.Printf("Read rule from file: %s\n", line)
+			if ! *jsonFlag {
+				fmt.Printf("Read rule from file: %s\n", line)
+			}
+
 			inputCh <- line
 		}
 
 		if err := scan.Err(); err != nil {
-			fmt.Fprintln(os.Stderr, "reading standard input:", err)
+			if ! *jsonFlag {
+				fmt.Fprintln(os.Stderr, "reading standard input:", err)
+			}
 			return
 		}
 	}()
@@ -37,7 +48,16 @@ func main() {
 	outCh := nlurules.ParseAsync(inputCh, false, false)
 
 	for u := range outCh {
-		fmt.Printf("Received utterance %+v\n", u)
+		if *jsonFlag {
+		out, err := json.MarshalIndent(u, "", "  ")
+			if err != nil {
+				os.Exit(1)
+			}
+			fmt.Printf(string(out))
+		} else {
+			fmt.Printf("Received utterance %+v\n", u)
+		}
+
 	}
 
 	os.Exit(0)
